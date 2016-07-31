9835dfad3e85b0e82772bb524ae2a644de7569c2
vikassry <vikassry@gmail.com>
2016-07-31 23:06:06 +0530

    Start TODO app with react

---
+++ todo/public/javascripts/counter.js
@@ -0,0 +1,19 @@
+// var MyComponent = React.createClass({
+//   getInitialState : function(){
+//     return { count : 0 }
+//   },
+//   increment : function(){
+//     this.setState({count : this.state.count + 1 });
+//   },
+//   render : function(){
+//     return (
+//       <div>
+//       <span> {this.props.name}
+//       Count : {this.state.count}
+//       </span><br/>
+//       <button type='button' onClick={this.increment}>Increment</button>
+//       </div>
+//     );
+//   }
+// });
+ReactDOM.render(<MyComponent name='hello'/> , document.getElementById('container'));
