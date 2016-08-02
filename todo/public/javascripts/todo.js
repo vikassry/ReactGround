87b5a8237062dc3098439a65046c9d0c797ff1d9
vikassry <vikassry@gmail.com>
2016-08-02 11:55:51 +0530

    render todo list

--- todo/public/javascripts/todo.js
+++ todo/public/javascripts/todo.js
@@ -1,9 +1,13 @@
 var TodoComponent = React.createClass({
   render : function(){
+    var todoList = [1,2,3,4,5]
     return (
-      
+      <ul> {todoList.map(function(x){
+        return <li key={x}>{x}</li>
+      })}
+      </ul>
     );
   }
 });
 
-ReactDOM.render(<p>toto</p>, document.getElementById('container'))
+ReactDOM.render(<TodoComponent/>, document.getElementById('container'))
