61a9bae3272f8dfa3079315bd502b8ef5ea7e552
vikassry <vikassry@gmail.com>
2016-08-03 17:38:39 +0530

    Reafactor

--- todo/public/javascripts/todo.js
+++ todo/public/javascripts/todo.js
@@ -1,32 +1,38 @@
 var TodoComponent = React.createClass({
+
   getInitialState : function(){
     return {tasks:[], text:''}
   },
+
   addTask : function(e){
     e.preventDefault();
-    var todo = this.state.text.trim();
+    var todo = this.state.text;
     if(isEmpty(todo)){
       alert("Can't add empty task!");
       return;
     }
-    var allTasks = this.state.tasks.concat({text:todo, id:Date.now()})
-    this.setState({tasks : allTasks, text:''});
+    var new_task = {text:todo, id:Date.now()};
+    var newTasks = this.state.tasks.concat(new_task);
+    this.setState({tasks : newTasks, text:''});
   },
+
   onChange : function(e){
     this.setState({text : e.target.value});
   },
+
   render : function(){
     var todoList = this.state.tasks;
     return (
       <div>
-      <ul> {todoList.map(function(item){
-        return <li key={item.id}>{item.text}</li>
-      })}
-      </ul>
-      <form onSubmit={this.addTask}>
-        <input onChange={this.onChange} value={this.state.text} />
-        <button>{'Add #' + (this.state.tasks.length + 1)}</button>
-      </form>
+        <ul>
+        {todoList.map(function(item){
+          return <li key={item.id}>{item.text}</li>
+        })}
+        </ul>
+        <form onSubmit={this.addTask}>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.tasks.length + 1)}</button>
+        </form>
       </div>
     );
   }
@@ -35,5 +41,5 @@ var TodoComponent = React.createClass({
 ReactDOM.render(<TodoComponent/>, document.getElementById('container'))
 
 var isEmpty = function(text) {
-  return text.trim().length ==0;
+  return text.trim().length == 0;
 }
