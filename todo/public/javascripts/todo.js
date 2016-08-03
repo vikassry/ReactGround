10f33806c06f4365337059318306106fdd5874a2
vikassry <vikassry@gmail.com>
2016-08-03 17:18:28 +0530

    TODO front end ready with react

--- todo/public/javascripts/todo.js
+++ todo/public/javascripts/todo.js
@@ -1,11 +1,28 @@
 var TodoComponent = React.createClass({
+  getInitialState : function(){
+    return {tasks:[], text:''}
+  },
+  addTask : function(e){
+    e.preventDefault();
+    var allTasks = this.state.tasks.concat({text:this.state.text, id:Date.now()})
+    this.setState({tasks : allTasks, text:''});
+  },
+  onChange : function(e){
+    this.setState({text : e.target.value});
+  },
   render : function(){
-    var todoList = [1,2,3,4,5]
+    var todoList = this.state.tasks;
     return (
-      <ul> {todoList.map(function(x){
-        return <li key={x}>{x}</li>
+      <div>
+      <ul> {todoList.map(function(item){
+        return <li key={item.id}>{item.text}</li>
       })}
       </ul>
+      <form onSubmit={this.addTask}>
+        <input onChange={this.onChange} value={this.state.text} />
+        <button>{'Add #' + (this.state.tasks.length + 1)}</button>
+      </form>
+      </div>
     );
   }
 });
